2e077f948b1ed1ee6e4f099e4f0ce656121f5336
Elie Halimi <elie.halimi@epita.fr>
2017-10-20 17:01:03 +0200

    made SDL work apt install libsdl-image1.2-dev added an image for testing

--- SDL/main.c
+++ SDL/main.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
-#include <err.h>
-#include <SDL.h>
-#include <SDL_image.h>
-#include "pixel_operations.h"
+// main.c
+
+# include <SDL/SDL.h>
+# include <SDL/SDL_image.h>
+# include <assert.h>
+# include <err.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include "pixel_operations.h"
 
 void wait_for_keypressed(void) {
   SDL_Event             event;
@@ -20,6 +24,7 @@ void wait_for_keypressed(void) {
   }
 }
 
+
 void init_sdl(void) {
   // Init only the video part
   if( SDL_Init(SDL_INIT_VIDEO)==-1 ) {
@@ -28,6 +33,7 @@ void init_sdl(void) {
   }
   // We don't really need a function for that ...
 }
+
 
 SDL_Surface* load_image(char *path) {
   SDL_Surface          *img;
@@ -39,7 +45,11 @@ SDL_Surface* load_image(char *path) {
   return img;
 }
 
-SDL_Surface* display_image(SDL_Surface *img) {
+
+
+
+SDL_Surface* display_image(SDL_Surface *img)
+{
   SDL_Surface          *screen;
   // Set the window to the same size as the image
   screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
@@ -48,45 +58,40 @@ SDL_Surface* display_image(SDL_Surface *img) {
     errx(1, "Couldn't set %dx%d video mode: %s\n",
          img->w, img->h, SDL_GetError());
   }
- 
+
   /* Blit onto the screen surface */
   if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
     warnx("BlitSurface error: %s\n", SDL_GetError());
- 
+
   // Update the screen
   SDL_UpdateRect(screen, 0, 0, img->w, img->h);
- 
+
+
   // wait for a key
   wait_for_keypressed();
- 
+
   // return the screen for further uses
   return screen;
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  init_sdl();
-  SDL_Surface *img = load_image(argv[1]);
-  display_image(img);
-  Uint8 r,g,b;
-  int width = img->w;
-  int height = img->h;
-  Uint32 pixel;
-  for (int i = 0; i < height; i++)
-  {
-    for (int j = 0; j < width; j++)
-    {
-      pixel = getpixel(img,i,j);
-      SDL_GetRGB(pixel, img->format, &r, &g, &b);
-      float rgb = r * 0.3 + g * 0.59 + b * 0.11;
-      r = rgb;
-      g = rgb;
-      b = rgb;
-      pixel = SDL_MapRGB(img->format, r, g, b);
-      putpixel(img, i, j, pixel);
-    }
-  }
-  display_image(img);
-  SDL_FreeSurface(img);
-  (void)argc;
+	if (argc != 2)
+		return -1;
+	SDL_Surface* img = load_image(argv[1]);
+	Uint8 r,g,b;
+	for (int i =0; i < img->w; i++)
+	{
+		for (int j =0; j< img->h; j++)
+		{
+			Uint32 pixel = getpixel(img, i, j);
+			SDL_GetRGB(pixel, img -> format, &r, &g, &b);
+			float luminance = (r*0.3 +g*0.59 +b*0.11);
+			pixel = SDL_MapRGB(img->format, luminance, luminance, luminance);
+			putpixel(img, i, j, pixel);
+		}
+	}
+	display_image(img);
+      	return 0;
 }
+
