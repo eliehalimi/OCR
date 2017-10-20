8a70354761dbbab34a5beefea3c268750c6a5ee1
Anatole Rousseau <anatole.rousseau@epita.fr>
2017-10-20 20:19:06 +0200

    Finished sample method for Voronoise

--- Voronoise/voronoise.c
+++ Voronoise/voronoise.c
@@ -5,9 +5,10 @@
 #include "SDL/pixel_operations.h"
 #include "SDL/pixel.operations.c"
 
-int take_samples[][](SDL_Surface *img, size_t x, size_t y)
+int[][] take_samples(SDL_Surface *img, size_t x, size_t y)
 {
-	int samples[][];
+	int samples[x * y / 2][1];
+	int k = 0;
 	Uint32 pixel;
 	Uint8 r, g, b;
 
@@ -16,7 +17,55 @@ int take_samples[][](SDL_Surface *img, size_t x, size_t y)
 		for (int j = 0; j < y; j++)
 		{
 			pixel = getpixel(img, i, j);
-			SDL_GetRGB(pixel, img->format, &r, &g, &b)
+			SDL_GetRGB(pixel, img->format, &r, &g, &b);
+			if (r == 255 && g == 255 && b == 255)
+			{
+				if (i != x - 1)
+				{
+					pixel = getpixel(img, i + 1, j);
+					SDL_GetRGB(pixel, img->format, &r, &g, &b);
+					if (r == 0 && g == 0 && b == 0)
+					{
+						samples[k][0] = i;
+						samples[k][1] = j;
+						k++;
+					}
+				}
+				if (i != 0)
+				{
+					pixel = getpixel(img, i - 1, j);
+					SDL_GetRGB(pixel, img->format, &r, &g, &b);
+					if (r == 0 && g == 0 && b == 0)
+					{
+						samples[k][0] = i;
+						samples[k][1] = j;
+						k++;
+					}
+				}
+				if (j != y - 1)
+				{
+					pixel = getpixel(img, i, j + 1);
+					SDL_GetRGB(pixel, img->format, &r, &g, &b);
+					if (r == 0 && g == 0 && b == 0)
+					{
+						samples[k][0] = i;
+						samples[k][1] = j;
+						k++;
+					}
+				}
+				if (j != 0)
+				{
+					pixel = getpixel(img, i, j + 1);
+					SDL_GetRGB(pixel, img->format, &r, &g, &b);
+					if (r == 0 && g == 0 && b == 0)
+					{
+						samples[k][0] = i;
+						samples[k][1] = j;
+						k++;
+					}
+				}
+			}
 		}
 	}
+	return samples;
 }
