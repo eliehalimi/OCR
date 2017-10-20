14d322631286439e995f5c6ee35f54d4b1cc977e
Elie Halimi <elie.halimi@epita.fr>
2017-10-20 22:20:59 +0200

    added save file should be able to open a file either with r or w parameter should be able to write some things in a file (predefined things for the time being) should be able to read contents

---
+++ save/save_file.h
@@ -0,0 +1,21 @@
+// pixel_operations.h
+ 
+# ifndef SAVE_FILE_H_
+# define SAVE_FILE_H_
+ 
+# include <stdlib.h>
+
+/* open a file with a parameter describing either read or write */ 
+void open_file(char *path, char *type);
+
+
+/* write to a file some text. Here predefined text.*/
+void write_to_file(FILE *f);
+//TODO add parameter (int *input) to add that value to the file
+
+
+/* reads content of a file */ 
+void read_file(FILE *f);
+
+
+# endif
