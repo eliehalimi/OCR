bf030fa199aeaa4671ce8f9bb4d6e916870bd5f2
Elie Halimi <elie.halimi@epita.fr>
2017-10-23 09:27:40 +0200

    header ne sert à rien

---
+++ nnet/test_function.h
@@ -0,0 +1,17 @@
+/* test_function.h : this contains the function that the neural network is compared to and the conversion function for the input and output */
+
+
+# ifndef TEST_FUNCTION_H_
+# define TEST_FUNCTION_H_
+
+double* generate_input(size_t epochs, size_t* size_begin,double* expected_begin);
+
+
+int main(int argc, char *argv[]);
+
+
+
+
+
+#endif
+
