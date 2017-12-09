02f6dbc78343dbf2a6cd8760e3cb27c78e6afbd3
Vincent Leflaive <vincent.leflaive@epita.fr>
2017-12-09 20:19:11 +0100

    A BREAAAAAAAAAKKK git add nnet_functions.

--- nnet/nnet_functions.c
+++ nnet/nnet_functions.c
@@ -199,7 +199,7 @@ void backprop(struct Neural_Net *nnet)
   	while(k < nnet->sizes_end - nnet->sizes_begin - 1) {
     	x = y;
    	y = nnet->layers_begin + *(nnet->sizes_begin + k) - 1;
-    	z = nnet->layers_begin + *(nnet->sizes_end -(5 - k))- 1);
+    	z = nnet->layers_begin + *(nnet->sizes_end -(5-k)) - 1;
     	backprop_layer(x,y,z);
     	k++;
   	}
