416b9e9b37c4179c8d3747915dc98655c59dc712
Vincent Leflaive <vincent.leflaive@epita.fr>
2017-12-10 18:16:07 +0100

    Je te demande de vivre Elie

--- nnet/nnet_functions.c
+++ nnet/nnet_functions.c
@@ -59,10 +59,10 @@ void layer_init(struct Sig_Neuron* layer_begin, struct Sig_Neuron* layer_end,
 	       	us the following expression.*/
     		
 	  (layer_begin+i)->weights_begin =
-	        (double*) malloc((layer_begin - 1 - prev_layer_begin) * sizeof(double));
+	        (double*) malloc((layer_begin - prev_layer_begin) * sizeof(double));
 	  (layer_begin+i)->weights_end =
 		(layer_begin+i)->weights_begin + (layer_begin - 1 - prev_layer_begin);
-		for(int j = 0; j < (layer_begin - 1 - prev_layer_begin); j++)
+		for(int j = 0; j < (layer_begin - prev_layer_begin); j++)
 	       	{
 		  (layer_begin + i)->weights_begin[j] = norm_dist(); 
     		}
@@ -108,10 +108,10 @@ void fflayer(struct Sig_Neuron* layer_begin, struct Sig_Neuron* layer_end,
 	for(int i = 0; i < layer_end - layer_begin; i++) 
 	{
 		(layer_begin + i)->output = 0;
-		for(int j = 0; j < (layer_begin - 1 - prev_layer_begin); j++) 
+		for(int j = 0; j < (layer_begin - prev_layer_begin); j++) 
 		{
-      			(layer_begin+i)->output += (prev_layer_begin+j)->output
-			  *((prev_layer_begin + j)->weights_begin[i]);
+      			(layer_begin + i)->output += (prev_layer_begin + j)->output *
+			  (prev_layer_begin + j)->weights_begin[i];
     		}
 		(layer_begin + i)->output = sigmoid((layer_begin + i)->output +
 				(layer_begin + i)->bias);
--- nnet/nnet_init.c
+++ nnet/nnet_init.c
@@ -22,7 +22,6 @@ struct Neural_Net* init(size_t* sizes_begin, size_t* sizes_end) {
   // neuron * the total number of neuron in the network */
   nnet->layers_begin = (struct Sig_Neuron*)
 	  malloc(sizeof(struct Sig_Neuron) * sum_sizes);
-  printf("%zu", *sizes_end);
   nnet->layers_end = nnet->layers_begin + sum_sizes;
   net_init(nnet);
   return nnet;
