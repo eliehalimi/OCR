9726ddde1f0b7e6bbc582e933a0f75b33cc4ccb8
Elie Halimi <elie.halimi@epita.fr>
2017-12-12 00:00:57 +0100

    added load doesn't work

--- save/save_file.c
+++ save/save_file.c
@@ -29,24 +29,134 @@ void open_file(char *path, char *type)
 
 void loadnnet(char* path)
 {
+	//compare sumsizes with sum (sum = layersize + other layer sizes) and if layersize + other = sumsizes then outputlayer
+
+	
+	
+
 	FILE *f = fopen(path, "r");
 	if (f == NULL)
 	{
 		printf("Error opening file!\n");
 		exit(1);
 	}
-	char * line = NULL;
-	size_t len = 0;
-	char read;
-
-	while ((read = getline(&line, &len, f)) != -1)
-	{
-		printf("%s\n", &read);
-		printf("%s", line);
-	}
-	if (line)
-		free(line);
 	
+	char* read = malloc(sizeof(char)*40);
+	int index = 0;
+	int status = 0;
+	int size_layer;
+	int word;
+	int c;
+	struct Neural_Net *nnet = (struct Neural_Net *) malloc(sizeof(struct Neural_Net));
+	nnet->sizes_begin = (size_t*) malloc(sizeof(size_t));
+	nnet->layers_begin = (struct Sig_Neuron *) malloc(sizeof(struct Sig_Neuron));
+	struct Sig_Neuron* cur;
+	int num_layer= 0;
+	int sum_sizes = 0;
+	int count = 0;
+	while ((c = fgetc(f)) != EOF)
+	{
+		if (status == 0)	//size_layer
+		{
+			if (c != (int)"\n")
+			{
+				*(read +index) += c;
+				i++;
+			}
+			else
+			{
+				size_layer = stoi(read, NULL);
+				status = 1;
+				index = 0;
+				nnet->sizes_begin[num_layer] = size_layer;
+				num_layer++;
+				count = 0;
+				sum_sizes += size_layer;
+				nnet->sizes_begin = realloc(sizeof(size_t) * num_layer);
+				nnet->layers_begin = realloc(sizeof(struct Sig_Neuron) * sum_sizes);
+			}
+		}
+		else if (status == 1)	//bias
+		{
+			cur++;
+			
+			if (c != "\n")
+			{
+				*(read + index) += c;
+				index++;
+			}
+			else
+			{
+				index = 0;
+				status = 2;
+				nnet->layers_begin[cur]->bias = strtod(read, NULL);
+
+			}
+		}
+		else
+		{
+			if (c != "," || c != "\n")
+			{
+				*(read + index) += c;
+				index++;
+			}
+			else
+			{
+				index = 0;
+				nnet->layers_begin[cur]->weights_begin = 
+					malloc(sizeof(double) * nnet->sizes_begin[num_layer - 1];
+				nnet->layers_begin[cur]->weights_begin[count] = strtod(read, NULL);
+				if(c == "\n")
+				{
+					if(idx == layer_size)
+						status = 0;
+					else
+						status = 1;
+				}
+				count++;
+			}
+		}
+/*
+
+			else if (c == ",")
+			{
+				size = read;
+   				index = 0;
+				//nnet->weight				
+			}
+			else 
+			{
+				size = read;
+				index = 0;
+				//nnet->weight
+				if (idx == layer_size)
+					status = 0;
+				else
+					status = 1;
+			}
+
+		}
+		
+*/		
+	}
+
+
+}
+	/*
+	char * read = malloc(sizeof(char));
+	//char *line = read;
+ 	char c = fgetc(f);
+	while (c != '\n' || c != EOF)
+	{
+		c = fgetc(f);
+		*read = c;
+		read++;
+	}
+	free(read);
+//	printf("%c", *line);	
+	
+	
+		
 
 	//int c;
 	//while(1)
@@ -57,12 +167,10 @@ void loadnnet(char* path)
 	//	       break;
 	//	}
 	//	printf("%c", c);
-	//}
+	//}*/
 	//fclose(f);
 
 
-
-}
 
 
 
@@ -77,23 +185,23 @@ void savennet(struct Neural_Net* nnet, char* path)
 		printf("Error opening file!\n");
 		exit(1);
 	}
-	fprintf(f, "{\n");
+	//fprintf(f, "{\n");
 
 	int k = 0;
         struct Sig_Neuron* y = nnet->layers_begin;
         while(k < nnet->sizes_end - nnet->sizes_begin)
         {
           printf("size of layer: %zu\n",*(nnet->sizes_begin + k));
-         fprintf(f, "%zu\n, ",*(nnet->sizes_begin + k));
+         fprintf(f, "%zu\n",*(nnet->sizes_begin + k));
           for(size_t i = 0; i < *(nnet->sizes_begin + k); i++)
           {
 	    printf("bias = %f\n", (y + i)->bias);
-           fprintf(f, " %f, ", (y + i)->bias);
-	   fprintf(f, "\n");
+           fprintf(f, "%f\n", (y + i)->bias);
+	   //fprintf(f, "\n");
             if(k == 0)
             {
               printf("weights: %f\n",(*(y + i)->weights_begin));
-             fprintf(f, "%f, ",(*(y + i)->weights_begin));
+             fprintf(f, "%f,",(*(y + i)->weights_begin));
             }
             else
             {
@@ -107,8 +215,9 @@ void savennet(struct Neural_Net* nnet, char* path)
           }
           y = y + *(nnet->sizes_begin + k);
           k++;
+	  fprintf(f, "--\n");
         }
-	fprintf(f, "\n}");
+	fprintf(f, "\n");
 	fclose(f);
 }
 
