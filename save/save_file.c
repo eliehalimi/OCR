5d62f71a189864bbce9e7360ec213df072b4f9ff
Elie Halimi <elie.halimi@epita.fr>
2017-12-12 05:16:47 +0100

    Elie is a zombie or maybe a mummy : I don't know

--- save/save_file.c
+++ save/save_file.c
@@ -6,34 +6,12 @@
 #include <stdlib.h>
 #include "save_file.h"
 #include <unistd.h>
-/*
-
- / / SEE DECLARATION IN HEADER
-void open_file(char *path, char *type)
-{
-	if (*type == "w" || *type == "r")
-	{
-		FILE *f = fopen(*path, "w");
-       	 	if (f == NULL)
-        	{
-        	printf("Error opening file!\n");
-        	exit(1);
-        	}
-		return *f;
-	}
-	printf("Error opening file! open with r or w parameter\n")
-	exit(1);
-}
-*/
 
 
-void loadnnet(char* path)
+
+struct Neural_Net* loadnnet(char* path)
 {
 	//compare sumsizes with sum (sum = layersize + other layer sizes) and if layersize + other = sumsizes then outputlayer
-
-	
-	
-
 	FILE *f = fopen(path, "r");
 	if (f == NULL)
 	{
@@ -41,136 +19,101 @@ void loadnnet(char* path)
 		exit(1);
 	}
 	
-	char* read = malloc(sizeof(char)*40);
+	char* read = malloc(sizeof(char) * 9);
+	*read = 0;
 	int index = 0;
 	int status = 0;
-	int size_layer;
-	int word;
+	int size_layer = 1;
+	int size_prev_layer;
 	int c;
 	struct Neural_Net *nnet = (struct Neural_Net *) malloc(sizeof(struct Neural_Net));
 	nnet->sizes_begin = (size_t*) malloc(sizeof(size_t));
 	nnet->layers_begin = (struct Sig_Neuron *) malloc(sizeof(struct Sig_Neuron));
-	struct Sig_Neuron* cur;
+	int cur = 0;
 	int num_layer= 0;
 	int sum_sizes = 0;
 	int count = 0;
+	int idx;
+	char * ptr;
+
 	while ((c = fgetc(f)) != EOF)
 	{
 		if (status == 0)	//size_layer
 		{
-			if (c != (int)"\n")
+			if (c != '\n')
 			{
-				*(read +index) += c;
-				i++;
+				*(read +index) = c;
+				index++;
 			}
 			else
 			{
-				size_layer = stoi(read, NULL);
+				size_prev_layer = size_layer;
+				size_layer = (int) strtol(read,NULL,10);
 				status = 1;
+				index = 0;
+				for(; index <9 ;index++)
+					*(read+index) = -66;
 				index = 0;
 				nnet->sizes_begin[num_layer] = size_layer;
 				num_layer++;
 				count = 0;
 				sum_sizes += size_layer;
-				nnet->sizes_begin = realloc(sizeof(size_t) * num_layer);
-				nnet->layers_begin = realloc(sizeof(struct Sig_Neuron) * sum_sizes);
+				printf("%d", size_layer);
+				nnet->sizes_begin = realloc(nnet->sizes_begin, sizeof(size_t) * (num_layer+1));
+				nnet->layers_begin = realloc(nnet->layers_begin, sizeof(struct Sig_Neuron) * sum_sizes); 
 			}
 		}
 		else if (status == 1)	//bias
 		{
-			cur++;
-			
-			if (c != "\n")
+			if (c != '\n')
 			{
-				*(read + index) += c;
+				*(read + index) = c;
 				index++;
 			}
 			else
 			{
 				index = 0;
 				status = 2;
-				nnet->layers_begin[cur]->bias = strtod(read, NULL);
-
-			}
+				(nnet->layers_begin + cur)->bias = strtod(read, &ptr);
+				for(; index <9 ;index++)
+					*(read+index) = -66;
+				index = 0;
+				cur++;
+				count++;
+				idx = 0;
+			}	
 		}
 		else
 		{
-			if (c != "," || c != "\n")
+		
+
+			if (c != ',' && c != '\n')
 			{
-				*(read + index) += c;
+				*(read + index) = c;
 				index++;
 			}
 			else
 			{
 				index = 0;
-				nnet->layers_begin[cur]->weights_begin = 
-					malloc(sizeof(double) * nnet->sizes_begin[num_layer - 1];
-				nnet->layers_begin[cur]->weights_begin[count] = strtod(read, NULL);
-				if(c == "\n")
+				(nnet->layers_begin + cur-1)->weights_begin = 
+					malloc(sizeof(double) * size_prev_layer);
+				(nnet->layers_begin + cur-1)->weights_begin[idx] = strtod(read, NULL);
+				idx++;
+				if(c =='\n')
 				{
-					if(idx == layer_size)
+					if(count == size_layer)
 						status = 0;
 					else
 						status = 1;
-				}
-				count++;
-			}
-		}
-/*
-
-			else if (c == ",")
-			{
-				size = read;
-   				index = 0;
-				//nnet->weight				
-			}
-			else 
-			{
-				size = read;
+				}	
+				for(; index<9 ;index++)
+					*(read+index) = -66;
 				index = 0;
-				//nnet->weight
-				if (idx == layer_size)
-					status = 0;
-				else
-					status = 1;
 			}
-
 		}
-		
-*/		
 	}
-
-
+	return nnet;
 }
-	/*
-	char * read = malloc(sizeof(char));
-	//char *line = read;
- 	char c = fgetc(f);
-	while (c != '\n' || c != EOF)
-	{
-		c = fgetc(f);
-		*read = c;
-		read++;
-	}
-	free(read);
-//	printf("%c", *line);	
-	
-	
-		
-
-	//int c;
-	//while(1)
-	//{
-	//	c = fgetc(f);
-	//	if(feof(f))
-	  //     	{
-	//	       break;
-	//	}
-	//	printf("%c", c);
-	//}*/
-	//fclose(f);
-
-
 
 
 
@@ -201,21 +144,22 @@ void savennet(struct Neural_Net* nnet, char* path)
             if(k == 0)
             {
               printf("weights: %f\n",(*(y + i)->weights_begin));
-             fprintf(f, "%f,",(*(y + i)->weights_begin));
+             fprintf(f, "%f",(*(y + i)->weights_begin));
             }
             else
             {
-              for(size_t j = 0; j < *(nnet->sizes_begin + k - 1); j++)
+              for(size_t j = 0; j < *(nnet->sizes_begin + k - 1)-1; j++)
                 {
                   printf("weights: %f\n",((y + i)->weights_begin[j]));
-                 fprintf(f, "%f, ",((y + i)->weights_begin[j]));
+                 fprintf(f, "%f,",((y + i)->weights_begin[j]));
                 }
+	      fprintf(f, "%f", ((y+i)->weights_begin[*(nnet->sizes_begin + k - 1)-1]));
             }
             fprintf(f, "\n");
-          }
+         }
           y = y + *(nnet->sizes_begin + k);
           k++;
-	  fprintf(f, "--\n");
+	  //fprintf(f, "--\n");
         }
 	fprintf(f, "\n");
 	fclose(f);
